83fb022df64bc225997ce70bb2c2155b84da109b
AnochaPay <Anocha_ping@hotmail.com>
2024-05-30 16:07:11 +0700

    add

---
+++ Smr_waitReady/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Smr_waitReady {
+    class Program {
+        static void Main(string[] args) {
+            Wait wait = new Wait();
+
+            wait.getHead();
+            wait.SendAtk();
+
+            if(wait.head == Define.head1) {
+                wait.CheckFile2();
+            } else {
+                wait.CheckFile1();
+            }
+
+            wait.SendEnd();
+        }
+
+        public class Wait {
+            public string head { get; set; }
+
+            public void getHead() {
+                while (true) {
+                    try {
+                        head = File.ReadAllText(Path.head);
+                        break;
+                    } catch { Thread.Sleep(50); }
+                }
+                File.Delete(Path.head);
+            }
+            public void SendAtk() {
+                File.WriteAllText(Path.atk, string.Empty);
+            }
+            public void CheckFile1() {
+                while (true) {
+                    try {
+                        File.ReadAllText(Path.file1);
+                        Thread.Sleep(250);
+
+                    } catch {
+                        break;
+                    }
+                }
+            }
+            public void CheckFile2() {
+                while (true) {
+                    try {
+                        File.ReadAllText(Path.file2);
+                        Thread.Sleep(250);
+
+                    } catch {
+                        break;
+                    }
+                }
+            }
+            public void SendEnd() {
+                File.WriteAllText(Path.result1 + head + Path.result2, Define.resultPass);
+            }
+        }
+        public static class Path {
+            public static readonly string head = "../../config/head.txt";
+            public static readonly string atk = "call_exe_tric.txt";
+            public static readonly string file1 = "SmrWaitReady1.txt";
+            public static readonly string file2 = "SmrWaitReady2.txt";
+            public static readonly string result1 = "test_head_";
+            public static readonly string result2 = "_result.txt";
+        }
+        public static class Define {
+            public static readonly string head1 = "1";
+            public static readonly string resultPass = "PASS\r\nPASS";
+        }
+    }
+}
---
+++ Smr_waitReady/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Smr_waitReady")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Smr_waitReady")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4a8c14ab-40bd-452c-ab31-441b22bd3c21")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
